6566a98627883a5d652a47a06611b3860afc4d93
João Félix <gamesjfpca@gmail.com>
2024-04-24 02:25:25 -0300

    finalmente ta tudo feito hahah

---
+++ Atividade 1/variaveisOperadores.js
@@ -0,0 +1,18 @@
+const a = 2;
+const b = 3;
+const c = "2";
+const d = "3";
+
+let resultado = a + b;
+console.log('O resultado da soma entre ' + a + ' e ' + b + ' é ' + resultado);
+
+resultado = a - b; 
+console.log(`O resultado da subtração entre ${a} e ${b} é ${resultado}`);
+
+resultado = a * b;
+console.log(`O resultado da multiplicação entre ${a} e ${b} é ${resultado}`);
+
+resultado = a / b;
+console.log(`O resultado da divisão entre ${a} e ${b} é ${resultado}`);
+
+console.log(`O resultado da soma entre ${d} e ${e} é ${c + d}`);
